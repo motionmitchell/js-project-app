47663e9c48b78753cd86399c7739f961757e64cf
motionmitchell <motionmitchell@users.noreply.github.com>
2021-03-22 19:43:42 -0500

    adding a commit

--- scripts0.js
+++ scripts0.js
@@ -1,3 +1,4 @@
   let pokemonList = [name: "bulbasaur", type: "grass", height: "7"] ;
 let pokemonList = [name: "Charmeleon", type: "fire", height: "1.1"]
   let pokemonList[name: "squirtle", type: "water", height: "0.5"]
+//this is an edit commit//
