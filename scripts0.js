879ec7e47843e7eb363018a2a67d0ce69ce28e49
motionmitchell <motionmitchell@users.noreply.github.com>
2021-03-27 20:09:34 -0500

    Re adding the pokemon names
    I added the code for 1.3

--- scripts0.js
+++ scripts0.js
@@ -1,4 +1,13 @@
-  let pokemonList = [name: "bulbasaur", type: "grass", height: "7"] ;
-let pokemonList = [name: "Charmeleon", type: "fire", height: "1.1"]
-  let pokemonList[name: "squirtle", type: "water", height: "0.5"]
+let pokemonList = [
+  { name: "bulbasaur", height: 7, type: ["grass"] },
+  { name: "Charmeleon", height: 1.1, type: ["fire"] },
+  { name: "squirtle", height: 0.5, type: ["water"] }
+];
 //this is an edit commit//
+
+let pokemonSizeThreshold = 0.7;
+
+for(let i = 0; i <= pokemonList.length; i++){
+    let pokemonString = `${pokemonList[i].name} ( Height: ${ pokemonList[i].height} )`;
+    let pokeman = pokemonList[i].height >= pokemonSizeThreshold ? `<p> ${pokemonString} - Wow, that’s big! </p>` : `<p> ${pokemonString} </p>`;
+    document.write(pokeman);
