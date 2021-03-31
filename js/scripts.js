e57766b5f275ac2553250461ed39f2a4886f3acf
motionmitchell <motionmitchell@users.noreply.github.com>
2021-03-31 18:05:22 -0500

    Adding Javascript code
    I tried reading the javascript and learning it by writing it in

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,67 @@
+//1.5 update down//
+
+let pokemonRepository= (function(){
+  let pokemonList = [];
+
+  let pokemon = "";
+  
+  //1.5 update up//
+
 let pokemonList = [
   { name: "bulbasaur", height: .6, type: ["grass"] },
   { name: "Charmeleon", height: .6, type: ["fire"] },
   { name: "squirtle", height: 0.7, type: ["water"] }
 ];
-//this is an edit commit//
+//This is the newst code for 1.5 down//
 
-let pokemonSizeThreshold = 0.7;
+function add(item){
+  if(typeof item === "object"){
+  pokemonList.push(item)
+ }
+ else alert("not possible")
+};
 
-for(let i = 0; i <= pokemonList.length; i++){
-    let pokemonString = `${pokemonList[i].name} ( Height: ${ pokemonList[i].height} )`;
-    let pokeman = pokemonList[i].height >= pokemonSizeThreshold ? `<p> ${pokemonString} - Wow, that’s big! </p>` : `<p> ${pokemonString} </p>`;
-    document.write(pokeman);
+function getAll(){
+  return pokemonList
+}
+function addListItem(pokemon){
+  let listPokemon = document.querySelector(".pokemon-list");
+  let listItem = document.createElement("li")
+   let button = document.createElement("button");
+    button.innerText= pokemon.name ;
+    button.classList.add(".button-class")
+   listPokemon.appendChild(listItem);
+   listItem.appendChild(button);
+
+   button.addEventListener("click",function(event){
+       showDetails(pokemon)
+   });
   }
+function showDetails(pokemon){
+   console.log(pokemon.name)
+}
+
+return {
+  add: add,
+  getAll: getAll,
+  addListItem:addListItem
+}
+
+})();
+
+function findWater(user){
+if(user.type === "water"){
+  return user
+}
+}
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
+pokemonRepository.addListItem(pokemon);
+});
+
+
+let dragons=pokemonRepository.getAll().filter(findWater);
+
+
+//Newest code for 1.5 up//
